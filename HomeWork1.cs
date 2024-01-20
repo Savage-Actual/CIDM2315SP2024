016f9bf598af1173120688e73e4869a1c27d2988
Dakota <156851676+Savage-Actual@users.noreply.github.com>
2024-01-19 19:46:33 -0600

    Upload HomeWork1

---
+++ HomeWork1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program 
+{
+    public static void Main(string[] args) 
+    {
+        
+        double x = 2.5;
+        double y = 3.3;
+
+        
+        double z = 4*x*x+3*y;
+
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.WriteLine($"The value of Z is: {z}");
+    }
+}
