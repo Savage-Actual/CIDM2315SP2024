7a33fd7500491c8d61688a2d03d0d5f5e7f8564e
Dakota <156851676+Savage-Actual@users.noreply.github.com>
2024-01-27 11:00:41 -0600

    Upload HomeWork2

---
+++ HomeWork2Q1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a letter grade: ");
+        char grade = Console.ReadLine().ToUpper()[0];
+
+        int gpaPoints;
+
+        if (grade == 'A')
+        {
+            gpaPoints = 4;
+        }
+        else if (grade == 'B')
+        {
+            gpaPoints = 3;
+        }
+        else if (grade == 'C')
+        {
+            gpaPoints = 2;
+        }
+        else if (grade == 'D')
+        {
+            gpaPoints = 1;
+        }
+        else if (grade == 'F')
+        {
+            gpaPoints = 0;
+        }
+        else
+        {
+            Console.WriteLine("Wrong Letter Grade!");
+            return;
+        }
+
+        Console.WriteLine($"GPA Points: {gpaPoints}");
+    }
+}
---
+++ HomeWork2Q2.cs
@@ -0,0 +1,38 @@
+﻿namespace HomeWork2Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      Console.Write("Please input the first number: ");
+      int first = Convert.ToInt16(Console.ReadLine());  
+
+      Console.Write("Please input the second number: ");
+      int second = Convert.ToInt16(Console.ReadLine());
+
+      Console.Write("Please input the third number: ");
+      int third = Convert.ToInt16(Console.ReadLine());
+
+      int smallest = Smallest(first, second, third);
+
+      Console.WriteLine($"The smallest value is: {smallest}");
+    }
+     static int Smallest(int a, int b, int c)
+    {
+        if (a <= b && a <= c)
+        {
+            return a;
+        }
+        else if (b <= a && b <= c)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+}
+
+
+
