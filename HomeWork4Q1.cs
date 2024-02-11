8e91689369c37a37addeccdcc9766b2e1bfb74b9
Dakota <156851676+Savage-Actual@users.noreply.github.com>
2024-02-11 09:42:13 -0600

    HomeWork4 Upload

---
+++ HomeWork4Q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number (A): ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number (B): ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = FindLargestNumber(number1, number2);
+
+        Console.WriteLine("A = {0}; B = {1}", number1, number2);
+
+        Console.WriteLine("The largest number is: {2}", number1, number2, largestNumber);
+    }
+
+    static int FindLargestNumber(int num1, int num2)
+    {
+        return (num1 > num2) ? num1 : num2;
+    }
+}
---
+++ HomeWork4Q2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Enter a number: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter a direction (left/right): ");
+        string direction = Console.ReadLine();
+ 
+        MakeShape(N, direction);
+    }
+
+    static void MakeShape(int N, string direction)
+    {
+        Console.WriteLine("N is: {0}; Shape is {1}", N, direction);
+
+        if (direction == "left")
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        else if (direction == "right")
+        {
+            
+            for (int i = 1; i <= N; i++)
+            {
+                
+                for (int k = 1; k <= N - i; k++)
+                {
+                    Console.Write("  ");
+                }
+
+                
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+}
