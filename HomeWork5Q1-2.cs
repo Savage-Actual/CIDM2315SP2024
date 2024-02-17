2152eff8be0c19ed0a32c41ba1fd91bc301cc7c1
Dakota <156851676+Savage-Actual@users.noreply.github.com>
2024-02-17 11:22:33 -0600

    Upload Homework 5

---
+++ HomeWork5Q1-2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number (A): ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number (B): ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the third number (C): ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the forth number (D): ");
+        int number4 = Convert.ToInt32(Console.ReadLine());
+
+        int largestNumber = FindLargestNumber(number1, number2, number3, number4);
+
+        Console.WriteLine("A = {0}; B = {1}; C = {2}; D = {3}", number1, number2, number3, number4);
+
+        Console.WriteLine($"The largest number is: {largestNumber}");
+    }
+
+    static int FindLargestNumber(int a, int b, int c, int d)
+    {
+        int largest = a;
+
+        if (b > largest)
+        {
+            largest = b;
+        }
+
+        if (c > largest)
+        {
+            largest = c;
+        }
+
+        if (d > largest)
+        {
+            largest = d;
+        }
+
+        return largest;
+    }
+}
---
+++ HomeWork5Q3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        createAccount();
+    }
+
+    static bool checkAge(int birth_year)
+    {
+        int current_year = DateTime.Now.Year;
+        int age = current_year - birth_year;
+        return age >= 18;
+    }
+
+    static void createAccount()
+    {
+        Console.Write("Enter Your Username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter Your Password: ");
+        string password1 = Console.ReadLine();
+
+        Console.Write("Enter Your Password Again: ");
+        string password2 = Console.ReadLine();
+
+        Console.Write("Enter Your Birthyear: ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        if (checkAge(birthYear))
+        {
+            if (password1 == password2)
+            {
+                Console.WriteLine("Account is created successfully");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Could not create an account");
+        }
+    }
+}
